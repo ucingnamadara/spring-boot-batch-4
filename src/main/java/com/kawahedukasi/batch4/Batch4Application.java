27b6f3b32414bffa9f7aa09176272aaeda7f5d7b
Dana <ananda.pratama@ifg-life.id>
2022-10-24 21:15:48 +0700

    initialize

---
+++ src/main/java/com/kawahedukasi/batch4/Batch4Application.java
@@ -0,0 +1,13 @@
+package com.kawahedukasi.batch4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Batch4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kawahedukasi/batch4/Batch4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawahedukasi.batch4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
